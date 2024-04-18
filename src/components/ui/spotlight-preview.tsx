117fd19b049e3d0cb04a47877a551d85e383deba
yegayv <yegay@sheridancollege.ca>
2024-04-18 15:46:46 -0400

    Switched type strictness due to temporary measures

--- src/components/ui/spotlight-preview.tsx
+++ src/components/ui/spotlight-preview.tsx
@@ -1,11 +1,6 @@
-import React from "react";
-import { cn } from "@/utils/cn";
-import { Spotlight } from "../ui/spotlight";
-
 export function SpotlightPreview() {
   return (
     <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/30 antialiased bg-grid-white/[0.02] relative overflow-hidden">
-     
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Hello <br /> , I am Vadim.
